bc173856954d8255b810cfde56a242b00e9e6fb3
Marko Tibold <marko@tibold.nl>
2015-03-21 19:16:02 +0100

    A lot of golden keystrokes

--- hackaton_backend/frontendapi/serializers.py
+++ hackaton_backend/frontendapi/serializers.py
@@ -1,16 +1,14 @@
 from django.forms import widgets
 from rest_framework import serializers
-from models import User,Message,Car
+from models import User,Message
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
 
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
+
--- hackaton_backend/frontendapi/views.py
+++ hackaton_backend/frontendapi/views.py
@@ -1,32 +1,51 @@
-from django.shortcuts import render
-from autoscout.models import trip_entry
-from models import User, Car, Message
-from serializers import UserSerializer,CarSerializer,MessageSerializer
-from rest_framework import generics
+from models import User, Message
+from serializers import UserSerializer,MessageSerializer
+from rest_framework import generics, views, response
 
 
 # Create your views here.
 class UserMixin(object):
-    
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class MessageMixin(object):
-    
+
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
-class CarMixin(object):
-    
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-
-
-class CarList(CarMixin, generics.ListCreateAPIView):
-    pass
 
 class UserList(UserMixin, generics.ListCreateAPIView):
     pass
 
 class MessageList(MessageMixin, generics.ListCreateAPIView):
     pass
+
+
+class NearbyCarsList(generics.ListAPIView):
+
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        #FIXME filter nearby cars
+        return User.objects.all()
+
+class MyBehaviour(views.APIView):
+
+    def get(self, request):
+        my_behaviour = self.request.user.current_behaviour
+        return response.Response(my_behaviour)
+
+
+class MyLocation(views.APIView):
+
+    def get(self, request):
+        my_location = self.request.user.last_known_position
+        return response.Response(my_location.as_dict())
+
+
+class CarStatus(views.APIView):
+
+    def get(self, request):
+        return response.Response(request.user.engine_status)
