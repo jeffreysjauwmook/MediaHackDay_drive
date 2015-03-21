0c9174b8890b68cc62aebccea88a8528872222d8
unknown <J.Sjauw_mook@SUL50194.media.corporatead.net>
2015-03-21 22:24:06 +0100

    new layout  index.html and test pics

---
+++ js/animate.js
@@ -0,0 +1,9 @@
+$( ".js-toggle__top" ).click(function() {
+    $('.menu__top').toggleClass("open");
+});
+$( ".js-toggle__bottom" ).click(function() {
+    $('.menu__bottom').toggleClass("open");
+});
+$( ".notification__reply" ).click(function() {
+    $('.menu__bottom').toggleClass("open");
+});
