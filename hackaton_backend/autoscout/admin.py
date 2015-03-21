efcf45045f13b34a48586581b9661828e749a1c9
Jaroslaw Swierad <jaroslaw.swierad@sanoma.com>
2015-03-21 16:43:22 +0100

    autoscout model

--- hackaton_backend/autoscout/admin.py
+++ hackaton_backend/autoscout/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from autoscout.models import trip_entry
 
-# Register your models here.
+admin.site.register(trip_entry)
--- hackaton_backend/autoscout/models.py
+++ hackaton_backend/autoscout/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class trip_entry(models.Model):
+    vim = models.CharField(max_length=200)
+    timestamp = models.DateTimeField()
+    km_total = models.IntegerField(default=0)
+    km_diff = models.IntegerField(default=0)
+    fuel_total = models.DecimalField(decimal_places=2, max_digits=99999)
+    fuel_diff = models.DecimalField(decimal_places=2, max_digits=99999)
+    heavy = models.BooleanField(default=False)
+
+
+
+
+
+
