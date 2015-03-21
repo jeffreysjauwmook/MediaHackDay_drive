50b3d48b8bf7075eca311deb2e6c2fc31240ed78
Marko Tibold <marko@tibold.nl>
2015-03-21 17:38:58 +0100

    Added geosimple and some models + migrations

--- hackaton_backend/frontendapi/models.py
+++ hackaton_backend/frontendapi/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+import geosimple
+
+
+class User(AbstractUser):
+    last_known_position = geosimple.GeohashField(blank=True, null=True)
+    social_score = models.IntegerField(blank=True, null=True)
+    current_behaviour = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sender')
+    receiver = models.ForeignKey(User, related_name='receiver')
+    sent_at = models.DateTimeField()
+    delivered_at = models.DateTimeField()
+    payload = models.CharField(max_length=255)  # sorry, thanks, no problem, wtf
+    delivered = models.BooleanField(
+        help_text='Delivered to receiver',
+        default=False
+    )
+    in_reply_to = models.ForeignKey(
+        'Message', blank=True, null=True, related_name='follow_up_message'
+    )
+
+
+class Car(models.Model):
+    owner = models.ForeignKey(User)
+    engine_status = models.CharField(max_length=255)
