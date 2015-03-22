193f8b3472d9ddbd5cbc670ac026f18ad57669b6
R4YM3 <raymond@hetwebbureau.nl>
2015-03-22 05:11:14 +0100

    Network footer

--- hackathon_mobile/js/animate.js
+++ hackathon_mobile/js/animate.js
@@ -7,3 +7,21 @@ $( ".js-toggle__bottom" ).click(function() {
 $( ".notification__reply" ).click(function() {
     $('.menu__bottom.reply').toggleClass("open");
 });
+
+
+$(window).on("resize", function () {
+    networkWidth = $( ".network" ).width();
+    networkChildren = $(".network .network__user").length;
+
+    if(networkChildren < 5) {
+        networkDivide = networkWidth / networkChildren;
+        $(".network .network__user").css("width",networkDivide);
+    }else {
+        networkDivide = networkWidth * 0.22;
+        $(".network .network__user").css("width",networkDivide);
+    }
+}).resize();
+
+$( ".network" ).click(function() {
+    $('.menu__bottom.reply').toggleClass("open");
+});
