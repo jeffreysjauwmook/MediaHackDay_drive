5a9a7db1d41a87060f260d56d1fb21497d303292
Marko Tibold <marko@tibold.nl>
2015-03-22 03:07:06 +0100

    use viewset for user views

--- hackaton_backend/frontendapi/views.py
+++ hackaton_backend/frontendapi/views.py
@@ -1,22 +1,19 @@
 from models import User, Message
 from serializers import UserSerializer,MessageSerializer
-from rest_framework import generics, views, response
+from rest_framework import generics, views, response, mixins, viewsets
 from autoscout.models import trip_entry
 from django.shortcuts import get_object_or_404
 
-
-# Create your views here.
-class UserMixin(object):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 class MessageMixin(object):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
 
-class UserList(UserMixin, generics.ListCreateAPIView):
-    pass
+class UserViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 class MessageList(MessageMixin, generics.ListCreateAPIView):
     """
